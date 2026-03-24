28ab5b11ca9128493a4eb098a393512efe607000
PAI-Peganov <artyompeganov428@gmail.com>
2026-03-24 17:13:34 +0500

    3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,18 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+    res.send({
+        items: [
+            {
+                "id": 1,
+                "name": "Пеганов Артём"
+            },
+            {
+                "id": 2,
+                "name": "Дедков Иван"
+            }
+        ]
+    });
 });
 
 module.exports = router;
