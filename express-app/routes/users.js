22ef9561e66abb6e11346452192e2c88b7e722eb
durak-online <dedkovewan417@yandex.com>
2026-03-24 17:22:27 +0500

    post

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const users = {
+    items: [
+        {
+            "id": 1,
+            "name": "Пеганов Артём"
+        },
+        {
+            "id": 2,
+            "name": "Дедков Иван"
+        }
+    ]
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send({
-        items: [
-            {
-                "id": 1,
-                "name": "Пеганов Артём"
-            },
-            {
-                "id": 2,
-                "name": "Дедков Иван"
-            }
-        ]
-    });
+    res.send(users);
+});
+
+router.post('/', function(req, res, next) {
+    const newUser = req.body;
+    users.items.push(newUser);
+    res.status(201).json(newUser);
 });
 
 module.exports = router;
